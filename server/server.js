768694f838e2a6525a7f8fdeba440cb28169535a
Vinay U <vinayu@portqii.com>
2024-08-21 18:03:32 +0530

    socket cors ingnore

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
